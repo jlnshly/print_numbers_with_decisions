0feb6e2a0e525b238e23cc24e47b4d97b490c6dd
jlnshly <jelaineashleyramos@gmail.com>
2026-03-13 02:13:29 +0800

    Fixed code

--- display_duplicates.py
+++ display_duplicates.py
@@ -3,10 +3,11 @@
 #Set decision
 number_list = []
 for i in range (1, 11):
-    user_input = float(input("Enter a number: "))
+    user_input = int(input("Enter a number: "))
     number_list.append(user_input)
 duplicate_list = []
 for user_input in number_list:
-    if number_list.count(user_input) >= 2:
+    if number_list.count(user_input) >= 2 and user_input not in duplicate_list:
         duplicate_list.append(user_input)
-        print(user_input)
+for user_input in duplicate_list:
+    print(user_input)
